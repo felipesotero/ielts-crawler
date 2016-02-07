bba4e72ae3375408fb0ae1b9da1792becf34bfa7
Luiz Sotero <luiz_sotero@apple.com>
2016-02-07 11:23:54 -0300

    Script accepts arguments from the command line

--- ielts.py
+++ ielts.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
+"""Script to check for availability of IELTS exams on a given country, city for a given module."""
+import sys
 import requests
 from bs4 import BeautifulSoup
+
+import options
+
+
+# Default options:
+TOWN = 'Recife'
+MODULE = 2  # General Training
+COUNTRY = 11  # Brazil
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko)\
@@ -13,9 +23,28 @@ Chrome/48.0.2564.82 Safari/537.36',
 FORM_URL = 'https://ielts.britishcouncil.org/Default.aspx'
 
 
+def main(country=None, town=None, module=None):
+    """Main entry point for the script."""
+    # Prepare options
+    country = country if country else COUNTRY
+    town = town if town else TOWN
+    module = module if module else MODULE
+
+    # Instantiate the crawler and call all methods.
+    crawler = IeltsCrawler(HEADERS, country, town, module)
+    form_soup = crawler.get_country_form(FORM_URL)
+    country_form_response_url = FORM_URL
+    country_form_soup = crawler.fill_country_form_and_get_availability_form(form_soup, country_form_response_url)
+    availability_result_page_soup = crawler.fill_availability_form_and_get_result_page(country_form_soup)
+    print crawler.get_availability_from_result_page(availability_result_page_soup)
+
+
 class IeltsCrawler(object):
-    def __init__(self, headers):
-        # Prepare form request
+    def __init__(self, headers, country, town, module):
+        # Prepare form request and form options
+        self.country = country
+        self.town = town
+        self.module = module
         self.session = requests.Session()
         self.session.headers.update(headers)
 
@@ -26,8 +55,7 @@ class IeltsCrawler(object):
         return BeautifulSoup(country_form_response.content, "html.parser")
 
     def fill_country_form_and_get_availability_form(self, form_soup, country_form_response_url):
-        # Prepare to select Brazil
-        BRAZIL = 11
+        # Prepare to select Country
         VIEWSTATE = form_soup.find(id='__VIEWSTATE')['value']
         EVENTVALIDATION = form_soup.find(id='__EVENTVALIDATION')['value']
 
@@ -36,7 +64,7 @@ class IeltsCrawler(object):
             '__EVENTARGUMENT': '',
             '__VIEWSTATE': VIEWSTATE,
             '__EVENTVALIDATION': EVENTVALIDATION,
-            'ctl00$ContentPlaceHolder1$ddlCountry': BRAZIL,
+            'ctl00$ContentPlaceHolder1$ddlCountry': self.country,
             'ctl00$ContentPlaceHolder1$imgbRegisterBtn.x': 59,
             'ctl00$ContentPlaceHolder1$imgbRegisterBtn.y': 8
         }
@@ -50,8 +78,6 @@ class IeltsCrawler(object):
     def fill_availability_form_and_get_result_page(self, country_form_soup):
         # Prepare to select the month, city and module:
         EARLY_DATE = country_form_soup.find(id='ctl00_ContentPlaceHolder1_ddlDateMonthYear').find('option')['value']
-        TOWN = 'Recife'
-        MODULE = 2  # General Training
         VIEWSTATE = country_form_soup.find(id='__VIEWSTATE')['value']
         # EVENTVALIDATION = country_form_soup.find(id='__EVENTVALIDATION')['value']
 
@@ -60,8 +86,8 @@ class IeltsCrawler(object):
             '__EVENTARGUMENT': '',
             '__VIEWSTATE': VIEWSTATE,
             'ctl00$ContentPlaceHolder1$ddlDateMonthYear': EARLY_DATE,
-            'ctl00$ContentPlaceHolder1$ddlTownCityVenue': TOWN,
-            'ctl00$ContentPlaceHolder1$ddlModule': MODULE,
+            'ctl00$ContentPlaceHolder1$ddlTownCityVenue': self.town,
+            'ctl00$ContentPlaceHolder1$ddlModule': self.module,
             'ctl00$ContentPlaceHolder1$imgbSearch.x': 51,
             'ctl00$ContentPlaceHolder1$imgbSearch.y': 12
         }
@@ -83,12 +109,26 @@ class IeltsCrawler(object):
         return str(availabilities)
 
 
-# Instantiate the crawler and call all methods.
-crawler = IeltsCrawler(HEADERS)
-form_soup = crawler.get_country_form(FORM_URL)
-country_form_response_url = FORM_URL
-country_form_soup = crawler.fill_country_form_and_get_availability_form(form_soup, country_form_response_url)
-availability_result_page_soup = crawler.fill_availability_form_and_get_result_page(country_form_soup)
-print crawler.get_availability_from_result_page(availability_result_page_soup)
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) == 4:
+        # Check if country, city and module is listed in constant options.
+        country = args[1]
+        city = args[2]
+        module = args[3]
 
-# TODO: Parametrize the options (country, town, module)
+        # Check if values are in options.py constants:
+        try:
+            country = options.COUNTRIES[country]
+            city = options.CITIES[city]
+            module = options.MODULES[module]
+        except:
+            sys.exit('Country, City or Module option unavailable. Check spelling')
+        sys.exit(main(country, city, module))
+
+    elif len(args) == 1:
+        # Run with default options
+        sys.exit(main())
+    else:
+        sys.exit('You must provide the country, city and module in that order if you do not want\
+            to run the default values, eg.: Brazil Recife "General Training"')
